35c50ee9f4335ff5c5d6eec55a14eb6cc1d66636
Vojtech Toth <vojtech.toth@globallogic.com>
2025-01-29 16:54:32 +0100

    Added auth.

--- src/screens/home-screen.tsx
+++ src/screens/home-screen.tsx
@@ -1,6 +1,26 @@
+import {useEffect} from 'react';
 import {Button} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 export const HomeScreen = ({navigation}) => {
+  useEffect(() => {
+    auth()
+      .createUserWithEmailAndPassword('granko87@gmail.com', 'pondelok')
+      .then(() => {
+        console.log('User account created & signed in!');
+      })
+      .catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!');
+        }
+
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+        }
+
+        console.error(error);
+      });
+  });
   return (
     <Button
       title="Go to Jane's profile"
